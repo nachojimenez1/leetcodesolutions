cbe53eb4c25ca4b65d85fa3b6e366028491e1f77
nachojimenez1 <nachetejm2006@gmail.com>
2026-03-24 19:18:19 +0100

    Time: 24 ms (41.72%) | Memory: 16 MB (91.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        d = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+
+            if key in d:
+                d[key].append(word)
+            else:
+                d[key] = [word]
+
+        return list(d.values())
