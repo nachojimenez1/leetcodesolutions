b3b88f897a19416e18beaf8538b4425bbcaa8556
Nacho <nachetejm2006@gmail.com>
2026-08-23 00:47:23 +0200

    Time: 0 ms (100.00%) | Memory: 11 MB (74.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        std::map<char,int> mapa = {
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000}
-        };
-        int primero = mapa[s.at(s.length()-1)];
-        int res = primero;
-        int segundo;
-        for(int i = s.length()-2; i >= 0; i--){
-            segundo = mapa[s.at(i)];
-            
-            if(segundo >= primero){
-                res += segundo;
-            }
-            else{
-                res -= segundo;    
-            }
-            
-            primero = segundo;
-            
+        int valores[256] = {};
+        
+        valores['I'] = 1;
+        valores['V'] = 5;
+        valores['X'] = 10;
+        valores['L'] = 50;
+        valores['C'] = 100;
+        valores['D'] = 500;
+        valores['M'] = 1000;
+
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (valores[s[i]] < valores[s[i + 1]])
+                res -= valores[s[i]];
+            else
+                res += valores[s[i]];
         }
+
         return res;
+
     }
 };
