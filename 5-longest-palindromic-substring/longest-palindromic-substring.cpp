848f424a49b668b725e7c938bf6ac3db99f0842b
Nacho <nachetejm2006@gmail.com>
2026-08-27 12:48:15 +0200

    Time: 10 ms (59.65%) | Memory: 9.2 MB (91.87%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         
-        std::string res = "";
-        res += s[0];
         int cursor = 0;
+        int i;
+        int j;
+        int imax = 0;
+        int jmax = 1;
         
         while(cursor < s.length()-1){
-            std::string impar = expandir(cursor, cursor, s);
-            if(impar.length() > res.length()){
-                res = impar;
+            i = cursor;
+            j = cursor;
+            int impar = expandir(i, j, s);
+            if(impar > jmax - imax){
+                imax = i + 1;
+                jmax = j;
             }
-            std::string par = expandir(cursor, cursor+1, s);
-            if(par.length() > res.length()){
-                res = par;
+            i = cursor;
+            j = cursor + 1;
+            int par = expandir(i, j, s);
+            if(par > jmax - imax){
+                imax = i + 1;
+                jmax = j;
             }
             cursor++;
         }
-        return res;
+        return s.substr(imax, jmax-imax);
     }
 
-    std::string expandir(int i, int j, std::string& s){
-        std::string izq = "";
-        std::string der = "";
+    int expandir(int& i, int& j, std::string& s){
 
         while(i>=0 && j<s.length() && s[i] == s[j]){
-            if(i == j){
-                izq += s[i];
-            }else{
-                izq = s[i] + izq;
-                der += s[i];
-            }
             i--;
             j++;    
         }
 
-        return izq + der;
+        return j-i-1;
         
-    }    
+    } 
 };
